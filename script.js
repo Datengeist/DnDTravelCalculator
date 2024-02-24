3719d5fbd5cc8f4530853c2703dc5b224114f013
Datengeist <71186126+Datengeist@users.noreply.github.com>
2024-02-24 21:45:47 +0100

    Add content

---
+++ script.js
@@ -0,0 +1,75 @@
+const travelDuration = document.getElementById('travelduration')
+const pathLength = document.getElementById('pathlength')
+const travelType = document.getElementById('traveltype')
+const travelSpeed = document.getElementById('travelspeed')
+const customSpeed = document.getElementById('customspeed')
+
+const undergroundChecksbox = [document.getElementById('checkroad'), document.getElementById('checkdirtroad'), document.getElementById('checkgras'), document.getElementById('checkforest'), document.getElementById('checkjungel'), document.getElementById('checkdesert'), document.getElementById('checkswamp'), document.getElementById('checksnow'), document.getElementById('checkmountain'), document.getElementById('checkextrememountain')]
+const undergroundPercent = [document.getElementById('percentroad'), document.getElementById('percentdirtroad'), document.getElementById('percentgras'), document.getElementById('percentforest'), document.getElementById('percentjungel'), document.getElementById('percentdesert'), document.getElementById('percentswamp'), document.getElementById('percentsnow'), document.getElementById('percentmountain'), document.getElementById('percentextrememountain')]
+
+const calculateButton = document.getElementById('buttoncalculate')
+
+const solution = document.getElementById('solution')
+
+//Nach Checkboxen Testen
+for(let i = 0; i < undergroundChecksbox.length; i++){
+  undergroundChecksbox[i].addEventListener('change', () => {
+    if (undergroundChecksbox[i].checked) {
+      undergroundPercent[i].removeAttribute('disabled');
+    } else {
+      undergroundPercent[i].setAttribute('disabled', true); 
+    }
+  });
+}
+
+//Nach Reise geschwindigkeit
+travelSpeed.addEventListener('change', () =>{
+  if(travelSpeed.value == 'custom'){
+    document.getElementById('customspeed').style.display = 'inline';
+  }else{
+    document.getElementById('customspeed').style.display = 'none';
+  }
+})
+
+function checkForInput(){
+  if(travelDuration.value > 24 || travelDuration.value < 0 || !travelDuration.value){
+    console.error('Reisedauer überprüfen')
+    return 'Reisedauer pro Tag kann nicht sein'
+  }
+  if(pathLength.value <= 0){
+    console.error('Es gibt keine negative Länge')
+    return 'Länge der Wegen kann nicht sein'
+  }
+
+  if(travelSpeed.value == 'custom'){
+    if(isNaN(parseInt(customSpeed))){
+      console.error('Gib ne Reisegschwindigkeit ein')
+      return('Geschwindigkeit kann nicht sein')
+    }
+  }
+
+  let foo = 0;
+  for(let i = 0; i < undergroundChecksbox.length; i++){
+    if(undergroundChecksbox[i].checked && !isNaN(parseInt(undergroundPercent[i].value))){
+      if(undergroundPercent[i].value<0){
+        console.error('kann keine -% geben')
+        return 'kann keine -% geben'
+      }
+      foo += parseInt(undergroundPercent[i].value)
+    }
+  }
+  if(foo!=100){
+    console.error('Untergrund muss 100% sein')
+    return 'Untergrund muss 100% sein'
+  }
+  return 'passt'
+}
+
+function berechnen(){
+  let input = checkForInput()
+  if(input !== 'passt'){
+    solution.textContent = input
+  }else{
+    solution.textContent = 'richtigö'
+  }
+}
